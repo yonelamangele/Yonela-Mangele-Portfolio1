02e749b8efa305f315cbf0aea72e51a41d915bde
Yonela <yonelamnagele1700@gmail.com>
2024-09-18 10:18:32 +0200

    added a homepage

---
+++ src/views/HomeView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <h1> Welcome To My Portfolio </h1>
+    <h3> My Name Is </h3>
+    <h1> <b> Yonela Mangele </b> </h1>
+    <h3> An Aspiring Web Developer </h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
+
+<style scoped>
+  .home {
+    display: flex;
+    justify-content: center;
+    height: 100vh;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+  }
+h1, h3 {
+  color: #092635;
+}
+</style>
